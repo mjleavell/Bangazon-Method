13cfe83fa22e6e3ea1153065975f6c8a5d51b815
Maggie Leavell <mjleavell@gmail.com>
2019-03-08 19:17:49 -0600

    first commit

---
+++ Bangazon-Method/Bangazon-Method/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bangazon_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
